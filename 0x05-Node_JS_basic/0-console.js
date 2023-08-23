863771350667c2f5d15c0c72b41a7ff724fbfe3a
CloobTech <belkid98@gmail.com>
2023-08-23 15:29:05 +0100

    Node Js basis

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/**
+* Display message to Stdout
+* @param {String} msg to be displayed
+* @author Olamide Bello (cloobTech)
+*/
+const displayMessage = (e) => console.log(e);
+
+module.exports = displayMessage;
